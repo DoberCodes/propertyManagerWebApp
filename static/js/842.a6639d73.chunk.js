10e4fd6079e8bc374552ee97340aadfdb60fd3b5
Austin <austin.dober@gmail.com>
2026-01-30 01:26:47 -0500

    Updates

---
+++ static/js/842.a6639d73.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmyPropertyManager=self.webpackChunkmyPropertyManager||[]).push([[842],{842:(s,e,n)=>{n.r(e),n.d(e,{Browser:()=>r,BrowserWeb:()=>t});var o=n(403);class t extends o.E_{constructor(){super(),this._lastWindow=null}async open(s){this._lastWindow=window.open(s.url,s.windowName||"_blank")}async close(){return new Promise(((s,e)=>{null!=this._lastWindow?(this._lastWindow.close(),this._lastWindow=null,s()):e("No active window to close!")}))}}const r=new t}}]);
+//# sourceMappingURL=842.a6639d73.chunk.js.map
