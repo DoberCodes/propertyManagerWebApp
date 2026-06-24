0e776a4f9be5c99ce22c7b7a4c8e9417ed2c8486
Austin <austin.dober@gmail.com>
2026-06-24 01:15:37 -0400

    release: v2.6.4
    - Bump version to 2.6.4
    - Update app version check
    - Update Android versionCode and versionName
    - Update release notes
    - Build signed APK

--- src/utils/versionCheck.ts
+++ src/utils/versionCheck.ts
@@ -14,7 +14,7 @@ import { Capacitor } from '@capacitor/core';
 import { Browser } from '@capacitor/browser';
 import { store } from '../Redux/store';
 
-const CURRENT_APP_VERSION = '2.6.3'; // Should match package.json version
+const CURRENT_APP_VERSION = '2.6.4'; // Should match package.json version
 const STORAGE_KEY = 'app_version_check';
 const DISMISS_KEY = 'app_update_dismissed_version';
 
